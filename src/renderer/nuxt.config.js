108d21ad9ad9d4158ab3d8d55d7f6ab654f0426c
Jake Medler <jake@jakemedler.co.uk>
2022-04-03 17:15:18 +0100

    init

---
+++ src/renderer/nuxt.config.js
@@ -0,0 +1,27 @@
+/**
+ * By default, Nuxt.js is configured to cover most use cases.
+ * This default configuration can be overwritten in this file
+ * @link {https://nuxtjs.org/guide/configuration/}
+ */
+
+
+module.exports = {
+  ssr: false,
+  target: 'static',
+  head: {
+    title: 'midipedal',
+    meta: [{ charset: "utf-8" }]
+  },
+  loading: false,
+  plugins: [
+    
+    
+    
+  ],
+  buildModules: [
+    
+  ],
+  modules: [
+    
+  ],
+};
